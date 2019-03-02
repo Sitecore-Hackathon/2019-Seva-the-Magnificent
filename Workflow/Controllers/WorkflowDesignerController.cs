b46cdd9740ce4e3e6fd7f5d73e20de03dc79c281
larionovadaria <daria_larionova@epam.com>
2019-03-02 10:44:40 -0800

    workflow-designer-ui

---
+++ Workflow/Controllers/WorkflowDesignerController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sitecore.Data.Items;
+
+namespace Workflow.Controllers
+{
+    public class WorkflowDesignerViewModel
+    {
+        public IEnumerable<Item> Workflows { get; set; }
+    }
+
+    public class WorkflowDesignerController : Controller
+    {
+        public ActionResult Index()
+        {
+            var workflowsFolderPath = "/sitecore/system/Workflows";
+
+            var database = Sitecore.Configuration.Factory.GetDatabase("master");
+
+            var workflowRootItem = database.GetItem(workflowsFolderPath);
+
+            var model = new WorkflowDesignerViewModel
+            {
+                Workflows = workflowRootItem.GetChildren()
+            };
+                
+
+            return this.View("WorkflowDesigner", model);
+        }
+    }
+}
